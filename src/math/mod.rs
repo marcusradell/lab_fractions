3b623a232ffce5ca8371122df5ad08300bbc321b
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-26 21:48:34 +0100

    we now add non-zero to zero

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,7 +2,7 @@ pub struct Fraction(i64);
 
 impl Fraction {
     pub fn add(&self, other: Fraction) -> Fraction {
-        Fraction(0)
+        other
     }
 
     pub fn to_int(&self) -> i64 {
@@ -14,4 +14,10 @@ impl Fraction {
 fn add_zero_to_zero() {
     let result: Fraction = Fraction(0).add(Fraction(0));
     assert_eq!(0, result.to_int());
+}
+
+#[test]
+fn add_non_zero_to_zero() {
+    let result: Fraction = Fraction(0).add(Fraction(3));
+    assert_eq!(3, result.to_int())
 }
