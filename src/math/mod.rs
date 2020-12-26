10016b7aa02a2c86a4f21c17287cc908b5be6ae4
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-26 22:37:39 +0100

    Remove the reference to add fn's second param.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,7 @@
 pub struct Fraction(i64);
 
 impl Fraction {
-    pub fn add(self, other: &Fraction) -> Fraction {
+    pub fn add(self, other: Fraction) -> Fraction {
         Fraction(self.0 + other.0)
     }
 
@@ -12,24 +12,24 @@ impl Fraction {
 
 #[test]
 fn add_zero_to_zero() {
-    let result = Fraction(0).add(&Fraction(0));
+    let result = Fraction(0).add(Fraction(0));
     assert_eq!(0, result.to_int());
 }
 
 #[test]
 fn add_non_zero_to_zero() {
-    let result = Fraction(0).add(&Fraction(3));
+    let result = Fraction(0).add(Fraction(3));
     assert_eq!(3, result.to_int())
 }
 
 #[test]
 fn add_zero_to_non_zero() {
-    let result = Fraction(5).add(&Fraction(0));
+    let result = Fraction(5).add(Fraction(0));
     assert_eq!(5, result.to_int())
 }
 
 #[test]
 fn non_zero_integers() {
-    let result = Fraction(3).add(&Fraction(4));
+    let result = Fraction(3).add(Fraction(4));
     assert_eq!(7, result.to_int())
 }
