71096db62f3a4f5aa5bc601c9719781d21a39ab4
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-26 22:19:09 +0100

    WIP: interesting case where we need to change the signature.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -3,7 +3,8 @@ pub struct Fraction(i64);
 impl Fraction {
     pub fn add<'a>(&'a self, other: &'a Fraction) -> &'a Fraction {
         if self.0 > 0 {
-            return self;
+            // return self;
+            return Fraction(self.0 + self.0);
         }
         &other
     }
@@ -29,4 +30,10 @@ fn add_non_zero_to_zero() {
 fn add_zero_to_non_zero() {
     let result: &Fraction = Fraction(5).add(&Fraction(0));
     assert_eq!(5, result.to_int())
+}
+
+#[test]
+fn non_negative_zero_operands() {
+    let result: &Fraction = Fraction(3).add(&Fraction(4));
+    assert_eq!(7, result.to_int())
 }
