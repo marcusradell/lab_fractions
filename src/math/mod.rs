c16de70b329ca4245aa68d71b0a16f3913e64167
Marcus Rådell <marcus.radell@sveasolar.com>
2020-12-28 15:10:57 +0100

    Update the code based on feedback from Rust Discord.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,14 +1,20 @@
-pub struct Fraction {
+#[allow(dead_code)]
+struct Fraction {
     numerator: i64,
     denominator: i64,
 }
 
+#[allow(dead_code)]
 impl Fraction {
-    pub fn new(numerator: i64) -> Fraction {
+    pub fn new(numerator: i64, denominator: i64) -> Fraction {
         Fraction {
             numerator,
-            denominator: 1,
+            denominator,
         }
+    }
+
+    pub fn from_int(numerator: i64) -> Fraction {
+        Fraction::new(numerator, 1)
     }
 
     pub fn add(self, other: Fraction) -> Fraction {
@@ -18,48 +24,42 @@ impl Fraction {
         }
     }
 
-    pub fn to_int(&self) -> i64 {
+    pub fn to_i64(&self) -> i64 {
         self.numerator
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn add_zero_to_zero() {
-        let result = Fraction::new(0).add(Fraction::new(0)).to_int();
+        let result = Fraction::from_int(0).add(Fraction::from_int(0)).to_i64();
         assert_eq!(0, result);
     }
 
     #[test]
     fn add_non_zero_to_zero() {
-        let result = Fraction::new(0).add(Fraction::new(3)).to_int();
+        let result = Fraction::from_int(0).add(Fraction::from_int(3)).to_i64();
         assert_eq!(3, result)
     }
 
     #[test]
     fn add_zero_to_non_zero() {
-        let result = Fraction::new(5).add(Fraction::new(0)).to_int();
+        let result = Fraction::from_int(5).add(Fraction::from_int(0)).to_i64();
         assert_eq!(5, result)
     }
 
     #[test]
     fn non_zero_integers() {
-        let result = Fraction::new(3).add(Fraction::new(4)).to_int();
+        let result = Fraction::from_int(3).add(Fraction::from_int(4)).to_i64();
         assert_eq!(7, result)
     }
 
     #[test]
     fn non_trivial_denominator() {
-        let result = Fraction {
-            numerator: 1,
-            denominator: 5,
-        }
-        .add(Fraction {
-            numerator: 2,
-            denominator: 5,
-        });
+        let result = Fraction::new(1, 5).add(Fraction::new(2, 5));
         assert_eq!(3, result.numerator);
         assert_eq!(5, result.denominator);
     }
