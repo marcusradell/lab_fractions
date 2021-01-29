9025e757b3bac295e92350db74fa28629cebde47
Marcus Rådell <marcus.radell@sveasolar.com>
2021-01-29 08:57:08 +0100

    Replaced assertions with equals check version.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -42,40 +42,31 @@ mod tests {
 
     #[test]
     fn add_zero_to_zero() {
-        let result = Fraction::from_int(0)
-            .add(Fraction::from_int(0))
-            .get_numerator();
-        assert_eq!(0, result);
+        let result = Fraction::from_int(0).add(Fraction::from_int(0));
+        assert_eq!(Fraction::from_int(0), result);
     }
 
     #[test]
     fn add_non_zero_to_zero() {
-        let result = Fraction::from_int(0)
-            .add(Fraction::from_int(3))
-            .get_numerator();
-        assert_eq!(3, result)
+        let result = Fraction::from_int(0).add(Fraction::from_int(3));
+        assert_eq!(Fraction::from_int(3), result);
     }
 
     #[test]
     fn add_zero_to_non_zero() {
-        let result = Fraction::from_int(5)
-            .add(Fraction::from_int(0))
-            .get_numerator();
-        assert_eq!(5, result)
+        let result = Fraction::from_int(5).add(Fraction::from_int(0));
+        assert_eq!(Fraction::from_int(5), result);
     }
 
     #[test]
     fn non_zero_integers() {
-        let result = Fraction::from_int(3)
-            .add(Fraction::from_int(4))
-            .get_numerator();
-        assert_eq!(7, result)
+        let result = Fraction::from_int(3).add(Fraction::from_int(4));
+        assert_eq!(Fraction::from_int(7), result);
     }
 
     #[test]
     fn non_trivial_denominator() {
         let result = Fraction::new(1, 5).add(Fraction::new(2, 5));
-        assert_eq!(3, result.get_numerator());
-        assert_eq!(5, result.get_denominator());
+        assert_eq!(Fraction::new(3, 5), result);
     }
 }
