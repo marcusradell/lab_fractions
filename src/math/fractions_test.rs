f7109548363586b80e168ab9d05688cdcbe3408a
Marcus Rådell <marcus.radell@sveasolar.com>
2021-02-01 08:37:10 +0100

    refactor

---
+++ src/math/fractions_test.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod tests {
+    use crate::math::Fraction;
+
+    #[test]
+    fn add_zero_to_zero() {
+        let expected = Fraction::from_int(0);
+        let result = Fraction::from_int(0).add(Fraction::from_int(0));
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn add_non_zero_to_zero() {
+        let expected = Fraction::from_int(3);
+        let result = Fraction::from_int(0).add(Fraction::from_int(3));
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn add_zero_to_non_zero() {
+        let expected = Fraction::from_int(5);
+        let result = Fraction::from_int(5).add(Fraction::from_int(0));
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn non_zero_integers() {
+        let expected = Fraction::from_int(7);
+        let result = Fraction::from_int(3).add(Fraction::from_int(4));
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn non_trivial_denominator() {
+        let expected = Fraction::new(3, 5);
+        let result = Fraction::new(1, 5).add(Fraction::new(2, 5));
+        assert_eq!(expected, result);
+    }
+}
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,4 +1,5 @@
-pub mod equals;
+mod equals_test;
+mod fractions_test;
 
 #[allow(dead_code)]
 #[derive(Debug, PartialEq, Eq)]
@@ -33,40 +34,5 @@ impl Fraction {
 
     pub fn get_denominator(&self) -> i64 {
         self.denominator
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_zero_to_zero() {
-        let result = Fraction::from_int(0).add(Fraction::from_int(0));
-        assert_eq!(Fraction::from_int(0), result);
-    }
-
-    #[test]
-    fn add_non_zero_to_zero() {
-        let result = Fraction::from_int(0).add(Fraction::from_int(3));
-        assert_eq!(Fraction::from_int(3), result);
-    }
-
-    #[test]
-    fn add_zero_to_non_zero() {
-        let result = Fraction::from_int(5).add(Fraction::from_int(0));
-        assert_eq!(Fraction::from_int(5), result);
-    }
-
-    #[test]
-    fn non_zero_integers() {
-        let result = Fraction::from_int(3).add(Fraction::from_int(4));
-        assert_eq!(Fraction::from_int(7), result);
-    }
-
-    #[test]
-    fn non_trivial_denominator() {
-        let result = Fraction::new(1, 5).add(Fraction::new(2, 5));
-        assert_eq!(Fraction::new(3, 5), result);
     }
 }
