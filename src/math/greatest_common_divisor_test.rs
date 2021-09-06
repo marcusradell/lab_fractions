4a3516b91a206d663c0ccbfa2d34e365858771a7
Marcus Rådell <marcus@radell.net>
2021-09-06 11:17:54 +0200

    GCD always positive.

--- src/math/greatest_common_divisor_test.rs
+++ src/math/greatest_common_divisor_test.rs
@@ -7,21 +7,43 @@ fn gcd(a: i64, b: i64) -> i64 {
         a = t;
     }
 
-    a
+    a.abs()
 }
 
 mod tests {
     use super::*;
 
     #[test]
-    fn one_and_one() {
-        assert_eq!(1, gcd(1, 1));
-    }
-
-    #[test]
     fn reflexive() {
         assert_eq!(1, gcd(1, 1));
         assert_eq!(2, gcd(2, 2));
-        assert_eq!(-1, gcd(-1, -1));
+        assert_eq!(1, gcd(-1, -1));
+    }
+
+    #[test]
+    fn relatively_prime() {
+        assert_eq!(1, gcd(2, 3));
+        assert_eq!(1, gcd(4, 7));
+        assert_eq!(1, gcd(-2, -3));
+    }
+
+    #[test]
+    fn one_is_multiple_of_the_other() {
+        assert_eq!(3, gcd(3, 9));
+        assert_eq!(5, gcd(5, 30));
+    }
+
+    #[test]
+    fn common_factor() {
+        assert_eq!(2, gcd(6, 8));
+        assert_eq!(7, gcd(49, 315));
+        assert_eq!(4, gcd(-24, -28));
+    }
+
+    #[test]
+    fn negatives() {
+        // GCD should always be positive.
+        assert_eq!(4, gcd(-24, 28));
+        assert_eq!(4, gcd(24, -28));
     }
 }
