832cb516d87ba78b0e84083ef1c78deb55c95a87
Marcus Rådell <marcus@radell.net>
2021-09-06 11:08:36 +0200

    Refactor gcd.

--- src/math/greatest_common_divisor_test.rs
+++ src/math/greatest_common_divisor_test.rs
@@ -1,13 +1,13 @@
 fn gcd(a: i64, b: i64) -> i64 {
-    let mut bb = b;
-    let mut aa = a;
-    while bb != 0 {
-        let t = bb;
-        bb = aa % t;
-        aa = t;
+    let mut b = b;
+    let mut a = a;
+    while b != 0 {
+        let t = b;
+        b = a % t;
+        a = t;
     }
 
-    aa
+    a
 }
 
 mod tests {
@@ -16,5 +16,12 @@ mod tests {
     #[test]
     fn one_and_one() {
         assert_eq!(1, gcd(1, 1));
+    }
+
+    #[test]
+    fn reflexive() {
+        assert_eq!(1, gcd(1, 1));
+        assert_eq!(2, gcd(2, 2));
+        assert_eq!(-1, gcd(-1, -1));
     }
 }
