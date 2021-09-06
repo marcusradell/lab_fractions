50f64c7658c90fef3959350ef11575c2e0874ec1
Marcus Rådell <marcus@radell.net>
2021-09-06 13:54:18 +0200

    Reduce zero test passes.

--- src/math/reduce_test.rs
+++ src/math/reduce_test.rs
@@ -11,4 +11,14 @@ mod tests {
     fn reduce_to_not_whole_numbers() {
         assert_eq!(Fraction::new(3, 4), Fraction::new(6, 8));
     }
+
+    #[test]
+    fn reduce_to_whole_numbers() {
+        assert_eq!(Fraction::from_int(6), Fraction::new(24, 4))
+    }
+
+    #[test]
+    fn reduce_zero() {
+        assert_eq!(Fraction::from_int(0), Fraction::new(0, 24654));
+    }
 }
